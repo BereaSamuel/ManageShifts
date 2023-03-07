2c41f1cc35e1b91d39e82b52aaaaabf973f1b283
AndreeaM0713 <andreea.mitroi28@gmail.com>
2023-03-07 11:51:59 +0200

    Components
    -am realizat componentele individuale pentru pagina de user si cea de admin

--- ManageShiftsProject/src/app/app.module.ts
+++ ManageShiftsProject/src/app/app.module.ts
@@ -7,10 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { RegistrationPageComponent } from './components/user-page/registration-page/registration-page.component';
+import { LoginPageComponent } from './components/user-page/login-page/login-page.component';
+import { HomePageComponent } from './components/user-page/home-page/home-page.component';
+import { ResetPasswordPageComponent } from './components/user-page/reset-password-page/reset-password-page.component';
+import { EditProfileComponent } from './components/user-page/edit-profile/edit-profile.component';
+import { ShiftsComponent } from './components/user-page/shifts/shifts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationPageComponent,
+    LoginPageComponent,
+    HomePageComponent,
+    ResetPasswordPageComponent,
+    EditProfileComponent,
+    ShiftsComponent
   ],
   imports: [
     BrowserModule,
