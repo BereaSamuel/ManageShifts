6663feade18d5159fc1370a9bd1f09bb108edcc7
AndreeaM0713 <andreea.mitroi28@gmail.com>
2023-03-07 13:58:34 +0200

    User page

--- ManageShiftsProject/src/app/components/user-page/home-page/home-page.component.ts
+++ ManageShiftsProject/src/app/components/user-page/home-page/home-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomepageComponent {
+  stored!: any
 
+  helloUser: string = '';
+
+  
+
+  ngOnInit(): void{
+    this.stored = localStorage.getItem('userInfoLS');
+    this.stored = JSON.parse(this.stored);
+  
+    if(this.stored){
+      this.helloUser = this.stored.username;
+    }
+
+  }
 }
