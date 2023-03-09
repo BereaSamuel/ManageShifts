b86db04804a53d6a3ae755ac7d8501cbdb5da5f6
AndreeaM0713 <andreea.mitroi28@gmail.com>
2023-03-09 14:19:59 +0200

    1
    -registration page
    -login page
    -reset password page

--- ManageShiftsProject/src/app/app.component.ts
+++ ManageShiftsProject/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ManageShiftsProject';
+
+  constructor(
+    private route: Router,
+    private toast: ToastrService
+  ){}
+
+  ngOnInit(): void {
+    this.route.navigate(['../registration-page']);
+  }
 }
--- ManageShiftsProject/src/app/app.module.ts
+++ ManageShiftsProject/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
@@ -9,7 +10,6 @@ import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
-import { AngularFireModule } from '@angular/fire/compat';
 
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
@@ -20,6 +20,18 @@ import { ShiftsAdminComponent } from './components/admin-page/shifts-admin/shift
 import { EditProfileAdminComponent } from './components/admin-page/edit-profile-admin/edit-profile-admin.component';
 import { ShiftsUserComponent } from './components/user-page/shifts-user/shifts-user.component';
 import { EditProfileUserComponent } from './components/user-page/edit-profile-user/edit-profile-user.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -32,17 +44,26 @@ import { EditProfileUserComponent } from './components/user-page/edit-profile-us
     ShiftsAdminComponent,
     EditProfileAdminComponent,
     ShiftsUserComponent,
-    EditProfileUserComponent,
+    EditProfileUserComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule,
+
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
-    AngularFirestoreModule,
-    FormsModule,
-    ReactiveFormsModule
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-center'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
